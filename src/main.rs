33e0d675e753fbe92828967f61b36c6d1c336dff
Ilya Maximov <mail@overlisted.net>
2022-01-30 04:22:22 +0100

    fix panic messages

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,15 @@ mod session;
 
 use crossterm::event::{KeyCode, KeyModifiers};
 use crossterm::{event, terminal};
-use std::{env, error, fs, io, process};
+use std::{env, error, fs, io, panic, process};
 use tui::layout::Rect;
+
+fn on_panic(info: &panic::PanicInfo) {
+    crossterm::execute!(io::stderr(), terminal::LeaveAlternateScreen).unwrap();
+    terminal::disable_raw_mode().unwrap();
+
+    println!("{}", info);
+}
 
 fn main() -> Result<(), Box<dyn error::Error>> {
     let mut args = env::args().skip(1);
@@ -26,6 +33,8 @@ fn main() -> Result<(), Box<dyn error::Error>> {
 
     terminal::enable_raw_mode()?;
     crossterm::execute!(stdout, terminal::EnterAlternateScreen)?;
+
+    panic::set_hook(Box::new(on_panic));
 
     let mut terminal = tui::Terminal::new(tui::backend::CrosstermBackend::new(io::stdout()))?;
     let mut session = session::Session::new(buffer);
