bf103b1e9b3b824ac4a76c5e3c0358dbe0a704b6
Ilya Maximov <mail@overlisted.net>
2022-01-30 04:22:40 +0100

    implement previews

--- src/session.rs
+++ src/session.rs
@@ -2,22 +2,27 @@ use crate::label::Label;
 use crate::line_table::LineTable;
 use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
 use regex::Regex;
+use std::fs;
+use std::ops::Range;
 use tui::*;
 
 pub struct Session {
     buffer: String,
     line_table: LineTable,
     prompt: String,
-    highlight: Vec<(usize, usize)>,
+    highlight: Vec<Vec<Range<usize>>>,
 }
 
 impl Session {
     pub fn new(buffer: String) -> Self {
+        let table = LineTable::new(&buffer);
+        let lines = table.len();
+
         Session {
-            line_table: LineTable::new(&buffer),
+            line_table: table,
             buffer,
             prompt: String::with_capacity(128),
-            highlight: Vec::new(),
+            highlight: vec![Vec::with_capacity(16); lines],
         }
     }
 
@@ -34,7 +39,9 @@ impl Session {
             //     }
             // }
             &["s", s, ""] => {
-                self.highlight.clear();
+                for line in &mut self.highlight {
+                    line.clear();
+                }
 
                 let regex = Regex::new(s).unwrap();
 
@@ -44,12 +51,39 @@ impl Session {
                 self.prompt.clear();
             }
             &["p", s, ""] => {
-                self.highlight.clear();
+                for line in &mut self.highlight {
+                    line.clear();
+                }
 
                 let regex = Regex::new(s).unwrap();
 
                 for m in regex.find_iter(&self.buffer) {
-                    self.highlight.push((m.start(), m.end()))
+                    let mut line_n = self.line_table.get_line_at(m.start());
+
+                    loop {
+                        let (line_start, line_end) = self.line_table.get_bounds(line_n);
+
+                        let final_range = Range {
+                            start: if line_start < m.start() {
+                                m.start()
+                            } else {
+                                line_start
+                            },
+                            end: if line_end > m.end() {
+                                m.end()
+                            } else {
+                                line_end
+                            },
+                        };
+
+                        self.highlight[line_n].push(final_range);
+
+                        if line_end >= m.end() {
+                            break;
+                        }
+
+                        line_n += 1;
+                    }
                 }
             }
             _ => {}
@@ -78,12 +112,24 @@ impl Session {
         let size = f.size();
 
         let mut y = 0;
-        for (_, range) in self.line_table.iter() {
-            let line = &self.buffer[range];
+        for (n, range) in self.line_table.iter() {
+            f.render_widget(
+                Label(&self.buffer[range.clone()], style::Style::default()),
+                layout::Rect::new(1, y, size.width - 1, 1),
+            );
 
-            let widget = Label(&line, style::Style::default());
+            for hl_range in &self.highlight[n] {
+                let x = hl_range.start as u16 - range.start as u16;
+                let width = hl_range.end as u16 - hl_range.start as u16;
 
-            f.render_widget(widget, layout::Rect::new(0, y, size.width, 1));
+                f.render_widget(
+                    Label(
+                        &self.buffer[hl_range.clone()],
+                        style::Style::default().bg(style::Color::LightYellow),
+                    ),
+                    layout::Rect::new(x, y, width, 1),
+                );
+            }
 
             y += 1;
         }
